7f1e51007fff170d067f4e6fc89329a04bd80b19
Nishek Sharma <sharmanishek@yahoo.com>
2026-01-25 21:13:00 +0530

    Insertion Sort Implemented

---
+++ Array/SortingTechniques/InsertionSort.java
@@ -0,0 +1,23 @@
+package Array.SortingTechniques;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    
+    public static void main(String[] args) {
+        int[] arr = {5,4,2,3};
+
+        for(int i = 1; i<arr.length; i++){
+            int key = arr[i];
+            int j = i - 1;
+            while(j>=0 && key < arr[j]){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j+1] = key;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
